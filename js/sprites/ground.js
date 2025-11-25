841d80a2deb9e9582259f15931767e9c3d619fa9
Noga Laufer <nogalaufer1@gmail.com>
2025-11-25 17:52:19 +0200

    ground

--- js/sprites/ground.js
+++ js/sprites/ground.js
@@ -1,5 +1,6 @@
 'use strict'
 let groundImg = new Image();
+// groundImg.src = './imgs/ground.frames3.png';
 groundImg.src = './imgs/ground.frames3.png';
 
 function restartGround() {
@@ -11,13 +12,17 @@ function restartGround() {
 
 const groundAmination = SpriteSheet({
     image: groundImg,
+    // frameWidth: 566,
     frameWidth: 566,
+    // frameHeight: 157,
     frameHeight: 163,
+    // frameHeight: groundImg.height/4,
 
 
     animations: {
         scroll: {
-            frames: [0, 1, 2,],
+            frames: [0, 1, 2],
+            // frames: "3",
             frameRate: 14,
         }
     }
@@ -25,7 +30,8 @@ const groundAmination = SpriteSheet({
 
 const ground = Sprite({
   x: 0,
-  y: 484,
+  // y: 484,
+  y: 487,
   scaleX: canvas.width / 566,
   scaleY: 0.8,
   animations: groundAmination.animations,
